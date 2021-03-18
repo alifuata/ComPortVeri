11b115e423fa7957a19c90930117380f69b15ce2
alifuata <53005120+alifuata@users.noreply.github.com>
2021-03-18 16:37:56 +0300

    first

--- ComPortVeri/Form1.Designer.cs
+++ ComPortVeri/Form1.Designer.cs
@@ -28,13 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cbComPorts = new System.Windows.Forms.ComboBox();
+            this.cbBaudRates = new System.Windows.Forms.ComboBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtSonuc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbComPorts
+            // 
+            this.cbComPorts.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.cbComPorts.FormattingEnabled = true;
+            this.cbComPorts.Location = new System.Drawing.Point(105, 15);
+            this.cbComPorts.Name = "cbComPorts";
+            this.cbComPorts.Size = new System.Drawing.Size(213, 26);
+            this.cbComPorts.TabIndex = 0;
+            // 
+            // cbBaudRates
+            // 
+            this.cbBaudRates.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.cbBaudRates.FormattingEnabled = true;
+            this.cbBaudRates.Location = new System.Drawing.Point(105, 56);
+            this.cbBaudRates.Name = "cbBaudRates";
+            this.cbBaudRates.Size = new System.Drawing.Size(213, 26);
+            this.cbBaudRates.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btnOpen.Location = new System.Drawing.Point(105, 88);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(213, 41);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Aç";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.txtSonuc.Location = new System.Drawing.Point(105, 144);
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.Size = new System.Drawing.Size(213, 24);
+            this.txtSonuc.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "COM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Baud Rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label3.Location = new System.Drawing.Point(12, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Gelen Değer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(337, 191);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSonuc);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.cbBaudRates);
+            this.Controls.Add(this.cbComPorts);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Afa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbComPorts;
+        private System.Windows.Forms.ComboBox cbBaudRates;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtSonuc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ComPortVeri/Form1.cs
+++ ComPortVeri/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,60 @@ namespace ComPortVeri
         public Form1()
         {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        public bool portAcikMi { get; set; } = false;
+
+        private SerialPort bizimPort;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnOpen.Text = "Aç";
+            //açık portları alıyoruz
+            string[] ports = SerialPort.GetPortNames();
+            foreach (var item in ports)
+            {
+                cbComPorts.Items.Add(item);
+            }
+            if (ports.Length > 0) cbComPorts.SelectedIndex = 0;
+            //standart baudrateleri ekliyoruz
+            cbBaudRates.Items.AddRange(new string[] { "75", "110", "300", "1200", "2400", "4800", "9600", "19200", "38400", "57600", "115200" });
+            cbBaudRates.SelectedIndex = 6;
+
+        }
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (!portAcikMi)
+            {
+                try
+                {
+                    //com portu açıyoruz
+                    bizimPort = new SerialPort(cbComPorts.SelectedItem.ToString(), Convert.ToInt32(cbBaudRates.SelectedItem), Parity.None, 8, StopBits.One);
+                    bizimPort.Open();
+                    portAcikMi = true;
+                    btnOpen.Text = "Kapat";
+                    //arayüzün kilitlenmemesi için gelen veriyi okuduğumuz kısım thread içinde
+                    Thread thread = new Thread(new ThreadStart(() =>
+                    {
+                        while (portAcikMi)
+                        {
+                            txtSonuc.Text = bizimPort.ReadExisting();
+                        }
+
+                    }));
+                    thread.Start();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata:" + ex.Message);
+                }
+            }
+            else
+            {
+                portAcikMi = false;
+                bizimPort.Close();
+                btnOpen.Text = "Aç";
+            }
         }
     }
 }
